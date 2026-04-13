cf25c8b0aa39cf25966c8cce18bb17aef36015a9
mort-joe <morten@jorgensen.nu>
2026-04-13 14:43:57 +0200

    justering av import

--- Shopping-list-app/src/App.jsx
+++ Shopping-list-app/src/App.jsx
@@ -1223,18 +1223,9 @@ function App() {
     const selectedRecipes = sortedRecipeImportCatalog.filter(
       (recipe) => selectedImportRecipeIds[recipe.id] && recipe.userId !== user.id,
     )
-    const importableRecipes = selectedRecipes.filter(
-      (recipe) => !ownedRecipeRootIds.has(recipe.sharedRootRecipeId ?? recipe.id),
-    )
 
     if (!selectedRecipes.length) {
       setRecipeImportMessage('Velg minst én matrett å importere.')
-      setIsRecipeImportError(true)
-      return
-    }
-
-    if (!importableRecipes.length) {
-      setRecipeImportMessage('Valgte matretter finnes allerede i din profil.')
       setIsRecipeImportError(true)
       return
     }
@@ -1246,7 +1237,7 @@ function App() {
     try {
       const targetDefaultPeople = normalizeDefaultPeopleValue(accountDefaultPeople)
 
-      for (const sourceRecipe of importableRecipes) {
+      for (const sourceRecipe of selectedRecipes) {
         const sourceDefaultPeople = normalizeDefaultPeopleValue(catalogDefaultPeopleByUser[sourceRecipe.userId])
         const scaleFactor = targetDefaultPeople / sourceDefaultPeople
         const sharedRootRecipeId = sourceRecipe.sharedRootRecipeId ?? sourceRecipe.id
@@ -1314,12 +1305,7 @@ function App() {
       }
 
       setSelectedImportRecipeIds({})
-      const skippedCount = selectedRecipes.length - importableRecipes.length
-      setRecipeImportMessage(
-        skippedCount > 0
-          ? `${importableRecipes.length} matrett(er) importert. ${skippedCount} ble hoppet over fordi de allerede finnes i profilen din.`
-          : 'Valgte matretter er importert til din brukerprofil.',
-      )
+      setRecipeImportMessage('Valgte matretter er importert til din brukerprofil.')
       await loadData(user.id)
       await loadRecipeImportCatalog()
     } catch (importError) {
@@ -2248,12 +2234,12 @@ function App() {
                           return (
                             <label
                               key={`import-recipe-${recipe.id}`}
-                              className={`account-import-row ${isOwnRecipe || alreadyInProfile ? 'disabled' : ''}`}
+                              className={`account-import-row ${isOwnRecipe ? 'disabled' : ''}`}
                             >
                               <input
                                 type="checkbox"
                                 checked={Boolean(selectedImportRecipeIds[recipe.id])}
-                                disabled={isOwnRecipe || alreadyInProfile || isImportingRecipes}
+                                disabled={isOwnRecipe || isImportingRecipes}
                                 onChange={() => handleToggleImportRecipe(recipe.id)}
                               />
                               <div className="account-import-title-row">
